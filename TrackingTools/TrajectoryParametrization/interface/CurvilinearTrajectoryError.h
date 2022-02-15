7af43490094a1e3610e07e672eb8a1700f78deb4
Anthony LaTorre <alatorre@caltech.edu>
2022-02-15 12:25:53 -0800

    initial commit

--- TrackingTools/TrajectoryParametrization/interface/CurvilinearTrajectoryError.h
+++ TrackingTools/TrajectoryParametrization/interface/CurvilinearTrajectoryError.h
@@ -4,6 +4,170 @@
 #include "DataFormats/GeometrySurface/interface/TrivialError.h"
 #include "DataFormats/Math/interface/AlgebraicROOTObjects.h"
 #include "DataFormats/Math/interface/Error.h"
+#include "TVectorD.h"
+
+/* Compute the V = LDL^T factorization of the Matrix V.
+ *
+ * Taken from https://github.com/nojhan/cholesky/blob/master/cholesky.h. */
+int ldl(TMatrixD &V, TMatrixD &D)
+{
+    int i, j, k;
+    int N = V.GetNrows();
+    // example of an invertible matrix whose decomposition is undefined
+    assert(V(0,0) != 0); 
+    TMatrixD L(N,N);
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            D(i,j) = 0;
+            V(i,j) = 0;
+        }
+    }
+
+    D(0,0) = V(0,0);
+
+    for (j = 0; j < N; j++) {
+        L(j, j) = 1;
+
+        D(j,j) = V(j,j);
+        for (k = 0; k <= j-1; k++) {
+            D(j,j) -= L(j,k) * L(j,k) * D(k,k);
+        }
+
+        for (i = j+1; i < N; i++) {
+            for (k = 0; k <= j-1; k++) {
+                L(i,j) -= L(i,k)*L(j,k) * D(k,k);
+            }
+            L(i,j) /= D(j,j);
+        }
+    }
+    //this->_L = root(L, D);
+}
+
+
+/* Compute the final symetric matrix: _L = L D^1/2
+ * remember that V = ( L D^1/2) ( L D^1/2)^T
+ * the factorization is thus L*D^1/2 */
+TMatrixD root(TMatrixD& L, TMatrixD &D)
+{
+    int i;
+
+    // fortunately, the square root of a diagonal matrix is the square 
+    // root of all its elements
+    TMatrix sqrt_D = D;
+    for (i = 0; i < D.GetNrows(); i++) {
+        sqrt_D(i,i) = sqrt(D(i,i));
+    }
+
+    return L*sqrt_D;
+}
+
+bool isHermitian(TMatrixD A)
+{
+    int i, j;
+
+    if (A.GetNrows() != A.GetNcols()) {
+        fprintf(stderr, "error: A is not square!\n");
+        return false;
+    }
+
+    for (i = 0; i < A.GetNrows(); i++) {
+        for (j = 0; j < A.GetNcols(); j++) {
+            if (A(i,j) != A(j,i))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+TMatrixD eye(int N)
+{
+    int i, j;
+
+    TMatrixD D(N,N);
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            if (i == j)
+                D(i,j) = 1;
+            else
+                D(i,j) = 0;
+        }
+    }
+
+    return D;
+}
+
+TMatrixD eig(TMatrixD &V, , TVectorD &e)
+{
+    int i;
+    TMatrixD D = eye(V.GetNrows());
+
+    V.EigenVectors(e);
+    for (i = 0; i < V.GetNrows(); i++)
+        D(i,i) = e(i);
+
+    return D;
+}
+
+/* modchol_ldlt  Modified Cholesky algorithm based on LDL' factorization.
+ * [L D,P,D0] = modchol_ldlt(A,delta) computes a modified Cholesky
+ * factorization P*(A + E)*P' = L*D*L', where P is a permutation matrix, L is
+ * unit lower triangular, and D is block diagonal and positive definite with
+ * 1-by-1 and 2-by-2 diagonal blocks.  Thus A+E is symmetric positive definite,
+ * but E is not explicitly computed.  Also returned is a block diagonal D0 such
+ * that P*A*P' = L*D0*L'.  If A is sufficiently positive definite then E = 0
+ * and D = D0.  The algorithm sets the smallest eigenvalue of D to the
+ * tolerance delta, which defaults to sqrt(eps)*norm(A,'fro').  The LDL'
+ * factorization is compute using a symmetric form of rook pivoting proposed by
+ * Ashcraft, Grimes and Lewis.
+ * 
+ * Reference:
+ * S. H. Cheng and N. J. Higham. A modified Cholesky algorithm based on a
+ * symmetric indefinite factorization. SIAM J. Matrix Anal. Appl.,
+ * 19(4):1097-1110, 1998. doi:10.1137/S0895479896302898,
+ * 
+ * Authors: Bobby Cheng and Nick Higham, 1996; revised 2015. */
+int modchol_ldlt(TMatrixD A, double delta)
+{
+    int i, j, k, N;
+
+    if (!isHermitian(A)) {
+        fprintf(stderr, "Must supply symmetric matrix.");
+        return 1;
+    }
+
+    N = A.GetNrows();
+
+    TMatrix D(N,N);
+    TVectorD U(N);
+
+    ldl(A,D); 
+    TMatrixD DMC = eye(N);
+
+    // Modified Cholesky perturbations.
+    k = 1;
+    while (k <= N) {
+        if ((k == N) || D(k,k+1) == 0) { // 1-by-1 block
+            if (D(k,k) <= delta)
+                DMC(k,k) = delta;
+            else
+                DMC(k,k) = D(k,k);
+            k = k+1;
+        } else { // 2-by-2 block
+            E = D(k:k+1,k:k+1);
+            TMatrixDSub E = TMatrixDSub(D,k,k+1,k,k+1);
+            T = eig(E,U);
+            for (ii = 1; ii <= 2; ii++) {
+                if T(ii,ii) <= delta
+                    T(ii,ii) = delta;
+            }
+        }
+        temp = U*(T*U).Transpose();
+        DMC(k:k+1,k:k+1) = (temp + temp.Transpose())/2;  // Ensure symmetric.
+        k = k + 2;
+    }
+}
 
 /** Parametrization of the error matrix in the curvilinear frame.
  *  This frame is tangent to the track at the point of definition,
@@ -41,10 +205,48 @@ public:
    *  the same as the one described above.
    */
   CurvilinearTrajectoryError(const AlgebraicSymMatrix55& aCovarianceMatrix) :
-    theCovarianceMatrix(aCovarianceMatrix) { }
+    theCovarianceMatrix(aCovarianceMatrix) {
+    int i;
+    TVectorD eigValues(5);
+    // need to copy the matrix since diagonalization is not
+    // available for SMatrix but there must be a better way
+    // to copy the matrix ...
+    TMatrixDSym diag(5);
+    for (i=0; i<5; ++i ) {
+        for ( unsigned int j=0; j<5; ++j ) {
+            diag(i,j) = theCovarianceMatrix(i,j);
+        }
+    }
+  
+    diag.EigenVectors(eigValues);
+    for (i = 0; i < 5; i++) {
+        if (eigValues(i) < 0) {
+            fprintf(stderr, "negative eigenvalue in curvilinear trajectory error!\n");
+        }
+    }
+ }
   template<typename M55>
   CurvilinearTrajectoryError(const M55& aCovarianceMatrix) :
-    theCovarianceMatrix(aCovarianceMatrix) { }
+    theCovarianceMatrix(aCovarianceMatrix) {
+    int i;
+    TVectorD eigValues(5);
+    // need to copy the matrix since diagonalization is not
+    // available for SMatrix but there must be a better way
+    // to copy the matrix ...
+    TMatrixDSym diag(5);
+    for (i=0; i<5; ++i ) {
+        for ( unsigned int j=0; j<5; ++j ) {
+            diag(i,j) = theCovarianceMatrix(i,j);
+        }
+    }
+  
+    diag.EigenVectors(eigValues);
+    for (i = 0; i < 5; i++) {
+        if (eigValues(i) < 0) {
+            fprintf(stderr, "negative eigenvalue in curvilinear trajectory error!\n");
+        }
+    }
+ }
 
 
   bool invalid() const { return theCovarianceMatrix(0,0)<-1.e10;}
